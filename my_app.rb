2c9ee948e8586a18ebfd6b465f828f728a6bb15e
Katherine Bailey <katherine@katbailey.net>
2015-05-28 13:45:19 -0400

    Adding the required endpoints

--- my_app.rb
+++ my_app.rb
@@ -8,8 +8,52 @@ configure do
 end
 
 get '/' do
-  $redis.set("foo", "bar")
-  # => "OK"
-  $redis.get("foo")
-  # => "bar"
+  "O HAI!"
+end
+
+get '/strategies' do
+  my_strategies = []
+  strategies = $redis.lrange("strategies", 0, -1)
+  strategies.each do |s|
+    my_strategies.push(s)
+  end
+  my_strategies.to_json
+end
+
+delete '/strategies' do
+  $redis.del("strategies")
+  "deleted all strategies"
+end
+
+post '/strategies' do
+  return_message = {}
+    jdata = JSON.parse(params[:data],:symbolize_names => true)
+    error(400, "Bad Request") unless jdata.has_key?(:name) && jdata.has_key?(:cid) && jdata.has_key?(:options)
+    if jdata.has_key?(:name)
+      $redis.lpush("strategies", jdata[:name])
+      key_prefix = "strategies:#{jdata[:name]}"
+      $redis.set("#{key_prefix}:cid", jdata[:cid])
+      $redis.lpush("cids:#{jdata[:cid]}:strategies", jdata[:name])
+      $redis.set("#{key_prefix}:options", jdata[:options].join(','))
+      return_message[:status] = jdata[:options]
+    else
+      return_message[:status] = 'sorry'
+    end
+    return_message.to_json
+end
+
+get '/decision' do
+  error(400, "Bad Request") unless params[:cid] && params[:pid]
+  my_strategies = []
+  strategies = $redis.lrange("cids:#{params[:cid]}:strategies", 0, -1)
+  strategies.each do |s|
+    my_strategies.push(s)
+  end
+  items = $redis.get("strategies:#{my_strategies[0]}:options")
+  error(404, "No such container") if items.nil?
+  items_array = items.split(',')
+  choice = rand(items_array.length)
+  choices = []
+  choices.push(items_array[choice].strip())
+  choices.to_json
 end
