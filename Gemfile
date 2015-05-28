fca1b1ea06567d16d878c3f6be02f8dff16ec8ba
Katherine Bailey <katherine@katbailey.net>
2015-05-28 11:15:07 -0400

    Starting with redis

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 ruby '2.2.0'
 gem 'sinatra', '1.1.0'
 gem 'redis'
+gem 'raml_parser'
+gem 'json'
+gem 'redis-namespace'
--- my_app.rb
+++ my_app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'json'
+require 'redis-namespace'
 
 configure do
     require 'redis'
@@ -6,6 +8,8 @@ configure do
     $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
+r = Redis::Namespace.new(:ns, :redis => @r)
 get '/' do
+  @strategies = r.LRANGE("strategies", 0, -1)
   'Hello world!'
 end
