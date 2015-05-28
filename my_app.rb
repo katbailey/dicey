1bd343f2fbb2815cd4b232145a4dad03c09a86e2
Katherine Bailey <katherine@katbailey.net>
2015-05-28 09:52:08 -0400

    Adding redis config

--- my_app.rb
+++ my_app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+configure do
+    require 'redis'
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 get '/' do
   'Hello world!'
 end
